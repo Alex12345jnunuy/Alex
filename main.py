7e5c45f4a1b3a54d0f815c33c0a8ae06f7e72219
Alex <apsantiago2006@gmail.com>
2025-05-02 05:11:40 +0100

    Importa jsonify no main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 # DON'T CHANGE THIS !!!
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
 from src.models.reservation import db # Changed from user model
 from src.routes.reservation import reservation_bp
 from src.routes.chat import chat_bp
